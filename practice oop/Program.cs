47c2d06f93e8ec43658faece1f9f9a41e1655770
abcmehedi5 <abcmehedi5@gmail.com>
2024-08-26 23:07:49 +0600

    Add project files.

---
+++ practice oop/Program.cs
@@ -0,0 +1,36 @@
+﻿using practice_oop;
+
+namespace practiceoop
+{
+    class Summary : Calculation
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        // Constructor to initialize A and B
+        public Summary(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        // Method to get the sum using the inherited sum method
+        public int GetSum()
+        {
+            return sum(A, B);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Summary with values for a and b
+            Summary summary = new Summary(3, 2);
+
+            // Get the sum using the Summary class
+            int result = summary.GetSum();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ practice oop/calculation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice_oop
+{
+    internal class Calculation
+    {
+        public int sum(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
